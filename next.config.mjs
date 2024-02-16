987cf319122acb2a2627271333564087d03894f9
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-02-16 22:47:46 +0530

    Update next.config.mjs with new image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: "http",
-                hostname: "localhost",
-            },
-            {
-                protocol: "https",
-                hostname: "tests-production-0ce4.up.railway.app/",
-            }
-        ],
+        domains: ['localhost', 'tests-production-0ce4.up.railway.app'],
     },
 };
 
